6af1b2eeb0542e3cdcaeda14ebc49f9d57806b99
josepheastman <josephryan96@gmail.com>
2019-01-11 11:02:17 -0800

    Completed endpoints for actions & projects

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const server = express();
+
+const configureMiddleware = require('../config/middleware.js')
+const apiRouter = require('../api/apiRouter.js');
+
+
+configureMiddleware(server);
+
+server.use('/api', apiRouter);
+
+
+module.exports = server;
--- data/helpers/actionModel.js
+++ data/helpers/actionModel.js
@@ -9,7 +9,7 @@ module.exports = {
       return query
         .where('id', id)
         .first()
-        .then(action => mappers.actionToBody(action));
+        .then(action => action ? mappers.actionToBody(action) : null);
     }
 
     return query.then(actions => {
